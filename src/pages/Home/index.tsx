60c506ffb94f2209eef499a21aa42695028092a2
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2024-02-07 19:35:02 -0300

    configuração inicial do projeto

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+export default function Home() {
+  return (
+    <div>
+      <S.H1Title>ANGELS</S.H1Title>
+    </div>
+  );
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const H1Title = styled.h1`
+  color: red;
+`;
