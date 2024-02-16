e3ac3521900b1e0a2628277693c4dd9a3121da5f
Jamuelton <jamueltonangelim@gmail.com>
2024-02-16 16:53:45 -0300

    refactor: addin g input props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,39 @@
+import React from 'react';
 import * as S from './styles';
 
 interface InputProps {
   label?: string;
+  type?: string;
+  placeHolder?: string;
+  value?: string;
+  disabled?: boolean;
+  rightAdd?: React.ReactNode;
+  leftAdd?: React.ReactNode;
+  color?: string;
+  inputFunction?: React.ChangeEventHandler<HTMLInputElement>;
 }
-export const Input: React.FC<InputProps> = ({ label }) => {
+export const Input: React.FC<InputProps> = ({
+  label,
+  type,
+  placeHolder,
+  value,
+  disabled,
+  rightAdd,
+  leftAdd,
+  inputFunction
+}) => {
   return (
     <S.Container>
       {label && <S.Label>{label}</S.Label>}
-      <S.InputArea />
+      <S.InputArea
+        disabled={disabled}
+        type={type}
+        value={value}
+        placeholder={placeHolder}
+        suffix={rightAdd}
+        prefix={leftAdd}
+        onChange={inputFunction}
+      />
     </S.Container>
   );
 };
