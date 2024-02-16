bfb08825adc94cc1051fcec54281739c5a16dbba
Jamuelton <jamueltonangelim@gmail.com>
2024-02-16 15:30:17 -0300

    chore: adding principal structure of pregnant register screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import styled from 'styled-components';
 import './App.css';
 import { AppRoutes } from './routes';
 
+const Main = styled.main`
+  height: 100%;
+  width: 100%;
+`;
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <Main>
+      <AppRoutes />
+    </Main>
+  );
 }
 
 export default App;
--- src/pages/PregnantRegister/styles.ts
+++ src/pages/PregnantRegister/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  div {
+    width: 80%;
+    height: 80%;
+    background: rgba(177, 72, 138, 0.2);
+  }
+`;
+
+export const TopContainer = styled.section``;
+
+export const FormContainer = styled.section``;
