1f7c5f76554acf95cecfb29ec69c35d5faee29d7
Jamuelton <jamueltonangelim@gmail.com>
2024-02-16 15:40:38 -0300

    refactor: taking out close button temporaly

--- src/pages/PregnantRegister/index.tsx
+++ src/pages/PregnantRegister/index.tsx
@@ -1,17 +1,15 @@
-import { XCircle } from '@phosphor-icons/react';
 import * as S from './styles';
 import Logo from '../../assets/angelsLogo.svg';
 
 export function PregnantRegister() {
   return (
     <S.Container>
-      <div>
+      <S.Contente>
         <S.TopContainer>
-          <XCircle size={32} weight="fill" color="#B1488A" />
           <img src={Logo} alt="angels logo" />
         </S.TopContainer>
         <S.FormContainer>input</S.FormContainer>
-      </div>
+      </S.Contente>
     </S.Container>
   );
 }
--- src/pages/PregnantRegister/styles.ts
+++ src/pages/PregnantRegister/styles.ts
@@ -4,13 +4,21 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  div {
-    width: 80%;
-    height: 80%;
-    background: rgba(177, 72, 138, 0.2);
-  }
 `;
 
-export const TopContainer = styled.section``;
+export const Contente = styled.div`
+  width: 80%;
+  height: 80%;
+  background: rgba(177, 72, 138, 0.2);
+`;
+
+export const TopContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  div {
+  }
+`;
 
 export const FormContainer = styled.section``;
