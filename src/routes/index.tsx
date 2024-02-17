804857a8e4ddda86c60226e5b322aea7914c024e
Ana Laura <analaurass45@gmail.com>
2024-02-17 16:16:30 -0300

    refactor: updating routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import { PregnantRegister } from '../pages/PregnantRegister';
+import { PregnancyRegister } from '../pages/PregnancyRegister';
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pregnantResgister" element={<PregnantRegister />} />
+        <Route path="/pregnantRegister" element={<PregnantRegister />} />
+        <Route path="/pregnancyRegister" element={<PregnancyRegister />} />
       </Routes>
     </BrowserRouter>
   );
