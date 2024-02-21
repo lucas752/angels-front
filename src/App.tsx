a1819d29420676608f5c9ba31d55e3eaa8182486
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2024-02-20 21:01:14 -0300

    chore: voltando o Home no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
 import Home from './pages/Home';
-import PregnantInfo from './pages/PregnantInfo';
 
 function App() {
   return (
     <>
-      <PregnantInfo/>
+      <Home />
     </>
   );
 }
