3e5e04780bfc295cd3dfd6c8f0c10782c3933d40
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-02-26 08:29:35 -0300

    adding buttons to the Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,6 +9,10 @@ export const Header : React.FC<HeaderProps> = ({
     return (
         <S.Container>
             <S.Logo src={logo} alt="Angels logo" />
+            <S.Button>HOME</S.Button>
+            <S.Button>DASHBOARD</S.Button>
+            <S.Button>INFORMAÇÕES DA GESTANTE</S.Button>
+            <S.Button>CADASTRO DA GESTANTE</S.Button>
         </S.Container>
         )
     };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     background-color: white;
     display: flex;
-    flex-direction: row;
+    align-items: center;
 `;
 
 export const Logo = styled.img`
@@ -11,4 +11,13 @@ export const Logo = styled.img`
     height: 110px;
     margin-top: 5px;
     margin-bottom: 5px;
+`;
+
+export const Button = styled.button`
+  background-color: #b1488a;
+  color: #ffffff;
+  padding: 10px 20px;
+  margin-left: 10px;
+  border: none;
+  cursor: pointer;
 `;
