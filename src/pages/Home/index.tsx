f45a30c763ed64ecca31caf1e1224005f9571292
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-02-26 07:52:50 -0300

    adding Home background color
    to advance the process and to help get a sense of the Header space

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
       <S.H1Title>ANGELS</S.H1Title>
+      <S.Container>
+      </S.Container>
     </div>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  background-color: #B1488A;
+  height: 100vh;
+  width: 100vw;
+`;
+
 export const H1Title = styled.h1`
-  color: red;
+  color: #B1488A;
 `;
