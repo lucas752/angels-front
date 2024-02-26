a58afbd09ef53c77b11aec9c30a78624cb2bda11
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-02-26 07:39:19 -0300

    Creating Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles';
+
+interface HeaderProps {
+
+}
+
+export const Header : React.FC<HeaderProps> = ({
+    ...
+}) => {
+    return (
+        <S.Container>
+            ...
+        </S.Container>
+    )
+}
