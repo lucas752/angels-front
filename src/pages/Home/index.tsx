4f4e63d28705cca7b5ba1825071df0370a99b267
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-03-02 20:38:07 -0300

    2nd version: Header and Home
    I will make the final version next week

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+
 import { Header } from '../../components/Header';
 import * as S from './styles';
 
@@ -6,6 +7,13 @@ export default function Home() {
     <div>
       <S.Container>
         <Header />
+        <S.Content>
+          <h1>angels</h1>
+          <p>O sistema Angels é uma solução tecnológica que simplifica 
+            o processo de acompanhamento pré-natal e oferece uma 
+            plataforma centralizada para  o registro detalhado de dados
+             e o acompanhamento adequado de cada etapa da gestação.</p>
+        </S.Content>
       </S.Container>
     </div>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,30 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: #B1488A;
+background-image: linear-gradient(to bottom right, #F1488A, #B1488A);
   height: 100vh;
   width: 100vw;
 `;
 
-export const H1Title = styled.h1`
-  color: #B1488A;
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  font-family: 'Inter', sans-serif;
+  padding: 0 12rem; 
+
+  h1 {
+    font-weight: SemiBold;
+    font-size: 90px;
+    margin-top: 7rem;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 1.5rem;
+    margin: 0 100px 40px 100px; 
+  }
 `;
