d2f4190f5ba634069763189d1a488d46b24fec1a
Ana Laura <analaurass45@gmail.com>
2024-03-04 21:23:51 -0300

    refactor: adding register route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home';
 import { PregnantRegister } from '../pages/PregnantRegister';
 import { PregnancyFollowUp } from '../pages/PregnancyFollowUp';
 import PregnantInfo from '../pages/PregnantInfo';
+import PregnancyRegister from '../pages/PregnancyRegister';
 
 export const AppRoutes = () => {
   return (
@@ -10,7 +11,8 @@ export const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pregnantRegister" element={<PregnantRegister />} />
-        <Route path="/pregnancyRegister" element={<PregnancyFollowUp />} />
+        <Route path="/pregnancyFollowUp" element={<PregnancyFollowUp />} />
+        <Route path="/pregnancyRegister" element={<PregnancyRegister />} />
         <Route path="/pregnantInfo" element={<PregnantInfo />} />
       </Routes>
     </BrowserRouter>
