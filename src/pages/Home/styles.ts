35d245bc82e52297c7580d754e32fd333254a8bf
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2024-03-09 15:57:06 -0300

    fix: fix gradient and make it responsive

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,30 +1,48 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-background-image: linear-gradient(to bottom right, #F1488A, #B1488A);
+  background: rgb(177, 72, 138);
+  background: linear-gradient(
+    90deg,
+    rgba(177, 72, 138, 1) 0%,
+    rgba(158, 16, 106, 1) 100%
+  );
   height: 100vh;
-  width: 100vw;
 `;
 
 export const Content = styled.div`
+  margin-top: 5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
+  gap: 2rem;
   color: #fff;
   font-family: 'Inter', sans-serif;
-  padding: 0 12rem; 
 
   h1 {
-    font-weight: SemiBold;
-    font-size: 90px;
-    margin-top: 7rem;
+    font-weight: 700;
+    font-size: 5.6rem;
     margin-bottom: 10px;
+
+    @media (max-width: 768px) {
+      font-size: 4.5rem;
+      margin-top: 4rem;
+      margin-bottom: 5px;
+    }
   }
 
   p {
     font-size: 1.5rem;
-    margin: 0 100px 40px 100px; 
+    text-align: center;
+    width: 70%;
+
+    @media (max-width: 768px) {
+      width: 90%;
+      margin-top: 4rem;
+      font-size: 1.2rem;
+      margin: 0 20px 20px 20px;
+    }
   }
 `;
